9fd49dd6958681ce4acd66da45fad84ebb309612
Damien Deturck <deturckdamien@gmail.com>
2021-08-27 15:34:48 +0200

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require("cors")
 
-const app = express()
+const app = express() 
 
 let corsOptions = {
     origin: "http://localhost:8081"
