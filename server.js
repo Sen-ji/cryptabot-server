319d3637fd022f4a7dcb663b8589bb9cfc337ee8
Damien Deturck <deturckdamien@gmail.com>
2021-08-27 15:19:40 +0200

    server cryptabot

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require("cors")
+
+const app = express()
+
+let corsOptions = {
+    origin: "http://localhost:8081"
+}
+app.use(cors(corsOptions))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const db = require("./models")
+const Role = db.role
+db.sequelize.sync({force: true}).then(()=>{
+    console.log("drop and sync DB");
+    initial();
+})
+
+app.get("/",(req,res) =>{
+    res.json({
+        message:"Bienvenue sur le serveur cryptabot"
+    })
+})
+
+app.listen(8080, ()=>{
+    console.log("Server Start")
+})
+
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
